2f31955bca0f2ab632143c336a3ed480015122a8
Parth Agrawal <pvagrawal0@gmail.com>
2024-06-03 14:06:36 -0400

    added daisyui and added carousel template

--- app/routes/portfolio.tsx
+++ app/routes/portfolio.tsx
@@ -1,16 +1,45 @@
-export default function Portfolio () {
+export default function Portfolio() {
 
-    return(
+    return (
         <div>
-            <h1 className="text-3xl font-bold" > 
-            Portfolio
+            <h1 className="text-3xl font-bold" >
+                Portfolio
             </h1>
 
             <ul className="list-disc">
                 <li> <a href="https://samosaman.itch.io/salami-game" className="underline" >Salami Game</a> - drop salami on a guy's head. That's definitely all that happens.</li>
-                <li> <a href="https://mmm.page/parth_agrawal.applied_cybernetics" className = "underline">Lilypads are solar panels</a></li>
+                <li> <a href="https://mmm.page/parth_agrawal.applied_cybernetics" className="underline">Lilypads are solar panels</a></li>
             </ul>
-
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
