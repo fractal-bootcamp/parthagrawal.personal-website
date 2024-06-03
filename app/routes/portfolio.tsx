c05a31d800ac45d236c9838e96dbe9e92f953fc2
Parth Agrawal <pvagrawal0@gmail.com>
2024-06-03 16:26:22 -0400

    carousel working'

--- app/routes/portfolio.tsx
+++ app/routes/portfolio.tsx
@@ -1,34 +1,91 @@
-const CarouselCard = ({ source, currentIndex, prevIndex, nextIndex }: {source: string, currentIndex: number, prevIndex: number, nextIndex: number}) => {
+import { ReactElement, ReactNode, useState } from "react"
 
-    return (<div id={'#slide' + currentIndex} className="carousel-item w-full">
-        <img src={source} className="carousel-item absolute h-full w-full" />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href={'#slide' + prevIndex} className="btn btn-circle">❮</a>
-            <a href={'#slide' + nextIndex} className="btn btn-circle">❯</a>
+
+const pictures = ["salamigame.png", "lilypads.png", "exojoint.png"]
+
+export function prevIndex(index: number, length: number) {
+    if (index == 0) {
+        return length - 1
+    }
+    return (index - 1) % length
+}
+export function nextIndex(index: number, length: number) {
+    return (index + 1) % length
+}
+// prevIndex(index, length) ==> (index - 1) % length
+// nextIndex(index, length) ==> (index + 1) % length
+
+interface CarouselItemProps {
+    index: number,
+    carouselLength: number,
+    picture: string
+}
+
+function myNamedFunction() {
+    return "foo"
+}
+
+
+const CarouselItem = ({ index, carouselLength, picture }: CarouselItemProps): ReactElement => {
+
+    return (
+        <div id={`slide${index}`} className="carousel-item relative w-full justify-center">
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img src={picture} /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href={`#slide${prevIndex(index, carouselLength)}`} className="btn btn-circle">❮</a>
+                <a href={`#slide${nextIndex(index, carouselLength)}`} className="btn btn-circle">❯</a>
+            </div>
         </div>
-    </div>)
+    )
+}
 
+function pictureToCarouselItem(picture: string, index: number): ReactElement {
+    const carouselLength = pictures.length
+    return <CarouselItem key={index} index={index} carouselLength={carouselLength} picture={picture} />
 }
 
 export default function Portfolio() {
 
     return (
-        <div>
-            <h1 className="text-3xl font-bold" >
-                Portfolio
-            </h1>
 
-            <ul className="list-disc">
-                <li> <a href="https://samosaman.itch.io/salami-game" className="underline" >Salami Game</a> - drop salami on a guy's head. That's definitely all that happens.</li>
-                <li> <a href="https://mmm.page/parth_agrawal.applied_cybernetics" className="underline">Lilypads are solar panels</a></li>
-            </ul>
-            <div className="carousel w-full">
-                <CarouselCard source = {"salamigame.png"} currentIndex = {1} prevIndex = {3} nextIndex = {2}/>
-                <CarouselCard source = {"lilypads.png"} currentIndex = {2} prevIndex = {1} nextIndex = {3}/>
-                <CarouselCard source = {"exojoint.png"} currentIndex = {3} prevIndex = {2} nextIndex = {1}/>
+        <div>
+            <h1 className="text-3xl font-bold">Portfolio</h1>
+
+
+            <div className="flex justify-center w-screen">
+
+
+
+                {/* Question: how do I add a variable (srcArray) to the carousel div that the carouselItem inherits  */}
+
+                <div className="carousel w-1/2" >
+
+                    {pictures.map(pictureToCarouselItem)}
+
+                    {/* <CarouselItem id={1} prevID={3} nextID={2} src="salamigame.png" />
+
+                    <CarouselItem id={2} prevID={1} nextID={3} src="lilypads.png" />
+
+                    <CarouselItem id={3} prevID={2} nextID={1} src="exojoint.png" /> */}
+
+
+
+                </div>
 
             </div>
+
+
         </div>
+
+
     )
 
 }
