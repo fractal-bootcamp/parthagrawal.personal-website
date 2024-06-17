2e317dab98c627c87c71d879048be864c1738fa8
Parth Agrawal <pvagrawal0@gmail.com>
2024-06-17 16:45:44 -0400

    scroll toy works kidna:

--- app/components/toy.tsx
+++ app/components/toy.tsx
@@ -1,25 +1,30 @@
+import { useScroll, useTransform, motion } from "framer-motion";
 import "./scroll.css"
-
-var r = document.querySelector(':root');
-
-// Create a function for getting a variable value
-function myFunction_get() {
-    // Get the styles (properties and values) for the root
-    if (r) var rs = getComputedStyle(r); else return;
-
-    // Alert the value of the --blue variable
-    alert("The value of --blue is: " + rs.getPropertyValue('--value'));
-}
-
+import { useEffect } from "react";
 
 
 export const Toy = () => {
 
+    const { scrollYProgress } = useScroll()
+    const radialProgress = useTransform(
+        scrollYProgress, [0, 1], [0, 100]
+    )
+    console.log(scrollYProgress)
+    const thing = Math.floor(scrollYProgress.get() * 100)
+
+    console.log('thing:', thing)
+
+
+
     return (
         <>
 
-            <div className="radial-progress" style={{ '--value': 70 } as React.CSSProperties} role="progressbar">70%</div>
+            <motion.div className="radial-progress" style={{ '--value': radialProgress } as React.CSSProperties} role="progressbar">70%</motion.div >
+            <div>
+                scrollYProgress: {scrollYProgress.get()}
+                thing: {thing}
 
+            </div>
         </>
     )
 }
